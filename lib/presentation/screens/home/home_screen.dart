4bfb724a43fc4a65ebbbfd7cdaa393c36830429c
Ernesto Alfonso Sanchez Montiel <ernest.alf45@gmail.com>
2024-04-23 17:19:43 -0600

    HomeScreen con ListTile

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 
 class HomeScreen extends StatelessWidget {
@@ -11,6 +12,59 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Flutter + Material 3'),
         
       ),
+      body: const _HomeView(),
     );
   }
 }
+
+class _HomeView extends StatelessWidget {
+
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ListView.builder(
+      
+      physics: const BouncingScrollPhysics(),
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);
+
+      },
+    );
+  }
+}
+
+
+// ListLile
+class _CustomListTile extends StatelessWidget {
+  final MenuItem menuItem;
+
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      trailing: Icon(Icons.arrow_forward_ios_outlined, color: colors.primary,),
+      onTap: () {
+        // TODO: Navegar a otras pantallas
+        
+      },
+     
+    );
+  }
+}
+
+
