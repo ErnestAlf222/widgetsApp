5c4c90fd3f49ad4f0e4cc67114fc1ddea183537b
Ernesto Alfonso Sanchez Montiel <ernest.alf45@gmail.com>
2024-05-08 16:45:45 -0600

    menú lateral terminado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,47 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
-
+import 'package:widgets_app/presentation/widgets/slider_menu.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-    static const name = 'home_screen'; 
+  static const name = 'home_screen';
 
   @override
   Widget build(BuildContext context) {
 
+    final scaffoldKey =  GlobalKey<ScaffoldState>();
+
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
-        
       ),
       body: const _HomeView(),
+      drawer: SideMenu( scaffoldKey: scaffoldKey)
     );
   }
 }
 
 class _HomeView extends StatelessWidget {
-
   const _HomeView();
 
   @override
   Widget build(BuildContext context) {
-
     return ListView.builder(
-      
       physics: const BouncingScrollPhysics(),
       itemCount: appMenuItems.length,
       itemBuilder: (context, index) {
-        
         final menuItem = appMenuItems[index];
         return _CustomListTile(menuItem: menuItem);
-
       },
     );
   }
 }
-
 
 // ListLile
 class _CustomListTile extends StatelessWidget {
@@ -51,10 +48,8 @@ class _CustomListTile extends StatelessWidget {
     required this.menuItem,
   });
 
-
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
 
     return ListTile(
@@ -62,22 +57,9 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       trailing: Icon(Icons.arrow_forward_ios_outlined, color: colors.primary),
-
       onTap: () {
-
-        // Navigator.of(context).push(
-        //   MaterialPageRoute(
-        //     builder: (context) => const ButtonScreen(),
-        //   ),
-        // );
-        // Navigator.pushNamed(context, menuItem.link);
-        // context.pushNamed(CardsScreen.name);
         context.push(menuItem.link);
-        
       },
-     
     );
   }
 }
-
-
